c6d78cdefa66a56cdac270fb183933f88cbc2861
Anton Tayanovskyy (t0yv0) <anton.tayanovskyy@gmail.com>
2014-10-03 12:26:24 -0700

    Initial commit.

---
+++ PCL/PCL.fs
@@ -0,0 +1,11 @@
+﻿namespace PCL
+
+open System
+open System.Runtime.CompilerServices
+
+module Lib =
+
+    // This is the current workaround:
+    // [<MethodImplAttribute(MethodImplOptions.NoInlining)>]
+    let year (dt: DateTime) =
+        dt.Year
---
+++ run.cmd
@@ -0,0 +1,2 @@
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe User/User.fsproj /p:Configuration=Release
+User\bin\Release\User.exe
